88f70b1f27d100f9ca6a9f710618bb40cb23086f
DiasDias97Dias <dias.duisembayev@nu.edu.kz>
2019-02-19 20:10:00 +0600

    Original

--- app/src/main/java/com/example/google/weatherclouds/MapsActivity.java
+++ app/src/main/java/com/example/google/weatherclouds/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.example.google.weatherclouds;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +12,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
@@ -24,7 +29,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private TileProvider tileTemp;
     private TileProvider tileClouds;
     private GoogleMap mMap;
-    private int mode;
+    private String mode;
+    private Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,21 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        i=getIntent();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mode=0;
+        mode="default";
         // Move the camera
         LatLng astana = new LatLng(51, 71);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(astana, 2));
@@ -75,7 +73,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 /* Define the URL pattern for the tile images */
                 String s = String.format( "http://tile.openweathermap.org/map/clouds_new/%d/%d/%d.png?appid=246e0838dbbffd086e19c8cae2dec946",
                         zoom, x, y);
-
                 try {
                     return new URL(s);
                 } catch (MalformedURLException e) {
@@ -84,24 +81,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
         };
+        if( i.getExtras()!=null && i.getStringExtra("x").length()>0 ){
+            String msg=i.getStringExtra("msg");
+            addTile("default");
+            int x=Integer.valueOf(i.getStringExtra("x"));
+            int y=Integer.valueOf(i.getStringExtra("y"));
+            LatLng danger = new LatLng(x,y);
+            int height = 50;
+            int width = 50;
+            BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.icon);
+            Bitmap b=bitmapdraw.getBitmap();
+            Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+            googleMap.addMarker(new MarkerOptions().position(danger)
+                    .title(msg).icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(danger));
+        }
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                if (mode==10) mode=2;
-                mode++;
                 mMap.clear();
-                if (mode%3==1){
-                    Toast.makeText(getApplicationContext(),"Temperature Map", Toast.LENGTH_SHORT).show();
-                    TileOverlay tileOverlayTemp = mMap.addTileOverlay(new TileOverlayOptions()
-                            .tileProvider(tileTemp));
-                } else if (mode%3==2) {
-                    Toast.makeText(getApplicationContext(),"Clouds Map", Toast.LENGTH_SHORT).show();
-                    TileOverlay tileOverlayClouds = mMap.addTileOverlay(new TileOverlayOptions()
-                            .tileProvider(tileClouds));} else {
-                    Toast.makeText(getApplicationContext(),"Default", Toast.LENGTH_SHORT).show();
-                }
+                addTile(mode);
             }
         });
 
+    }
+
+    private void addTile(String mode){
+        if (mode.equals("default")){
+            this.mode="temp";
+            Toast.makeText(getApplicationContext(),"Temperature Map", Toast.LENGTH_SHORT).show();
+            TileOverlay tileOverlayTemp = mMap.addTileOverlay(new TileOverlayOptions()
+                    .tileProvider(tileTemp));
+        } else if (mode.equals("temp")) {
+            this.mode="clouds";
+            Toast.makeText(getApplicationContext(),"Clouds Map", Toast.LENGTH_SHORT).show();
+            TileOverlay tileOverlayClouds = mMap.addTileOverlay(new TileOverlayOptions()
+                    .tileProvider(tileClouds));} else if (mode.equals("clouds")){
+            this.mode="default";
+            Toast.makeText(getApplicationContext(),"Default", Toast.LENGTH_SHORT).show();
+        }
     }
 }
